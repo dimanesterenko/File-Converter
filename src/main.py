276f86819f425bb4b88480a38dac878ba7aaaca1
Dima Nesterenko <nesterenkoodmytro@gmail.com>
2024-01-10 16:00:42 +0200

    add pdf to csv implementation

--- src/main.py
+++ src/main.py
@@ -1,4 +1,33 @@
 from pdf2docx import Converter
+import tabula
+
 
 pdf_file="pdf-test.pdf"
 word_file = "word_test.docx"
+
+
+
+#converter method
+def pdf_to_word():
+    dc = Converter(pdf_file)
+    dc.convert(word_file, start=0, end=None)
+    dc.close()
+
+def pdf_to_csv():
+    data=tabula.convert_into("table.pdf","table_new.csv", pages="all",output_format="csv")
+
+def pdf_to_txt():
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
