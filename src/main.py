d01456119cf6e01a67845005ee3b4ee49215a53d
Dima Nesterenko <nesterenkoodmytro@gmail.com>
2024-01-10 22:09:54 +0200

    add pdf to txt implementatioan

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from pdf2docx import Converter
 import tabula
-
+import PyPDF2
 
 pdf_file="pdf-test.pdf"
 word_file = "word_test.docx"
@@ -17,17 +17,17 @@ def pdf_to_csv():
     data=tabula.convert_into("table.pdf","table_new.csv", pages="all",output_format="csv")
 
 def pdf_to_txt():
+    # pdf to txt
+    pdfFileObj = open("pdf-test.pdf", 'rb')
+
+    pdfReader = PyPDF2.PdfReader(pdfFileObj)
+    pageObject = pdfReader.pages[0]
+    text = pageObject.extract_text()
+    pdfFileObj.close()
+    with open("demo.txt", 'w') as file:
+        file.write(text)
+
+
+
+def pdf_to_images():
     pass
-
-
-
-
-
-
-
-
-
-
-
-
-
